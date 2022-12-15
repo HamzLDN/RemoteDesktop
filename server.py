da9159b6ef0ada6deb8c33f0117103c4e9174205
HamzLDN <75676416+HamzLDN@users.noreply.github.com>
2022-12-15 23:35:09 +0000

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
-import zlib
-import socket
 import cv2
 import numpy as np
+import socket
+import pickle
+import struct
+import threading
 import win32api
 import win32con
 import math
-# Monitors resolution
+
 WIDTH = win32api.GetSystemMetrics(0)
 HEIGHT = win32api.GetSystemMetrics(1)
 # 1920x1080 FOR MY SCREENs
@@ -13,52 +15,126 @@ HEIGHT = win32api.GetSystemMetrics(1)
 # Softwares resolution
 SWIDTH = 960
 SHEIGHT = 540
+class StreamingServer:
+    def __init__(self, host, port, slots=8, quit_key='q'):
+        self.__host = host
+        self.__port = port
+        self.__slots = slots
+        self.__used_slots = 0
+        self.__running = False
+        self.__block = threading.Lock()
+        self.__server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__init_socket()
 
-class servercontrol:
-    def __init__(self, ip: str = "0.0.0.0", port: int = 443):
-        self.ip = ip
-        self.port = port
-        self.socket = None
-        self.connect()
+    def __init_socket(self):
+        """
+        Binds the server socket to the given host and port
+        """
+        self.__server_socket.bind((self.__host, self.__port))
 
-    def recvdata(self, buffer: int=4096, data: bytes=b''):
-        while True:
-            part = self.socket.recv(buffer)
-            data += part
-            if len(part) < buffer:
-                break
-        return data
+    def start_server(self):
+        """
+        Starts the server if it is not running already.
+        """
+        if self.__running:
+            print("Server is already running")
+        else:
+            self.__running = True
+            server_thread = threading.Thread(target=self.__server_listening)
+            server_thread.start()
 
-    def showcords(self, event, x, y, flags, params) -> int:
+    def __server_listening(self):
+        """
+        Listens for new connections.
+        """
+        global connection
+        self.__server_socket.listen()
+        while self.__running:
+            self.__block.acquire()
+            connection, address = self.__server_socket.accept()
+            if self.__used_slots >= self.__slots:
+                print("Connection refused! No free slots!")
+                connection.close()
+                self.__block.release()
+                continue
+            else:
+                self.__used_slots += 1
+            self.__block.release()
+            thread = threading.Thread(target=self.__client_connection, args=(connection, address,))
+            thread.start()
+
+            print("Connection started!")
+
+    def stop_server(self):
+        """
+        Stops the server and closes all connections
+        """
+        if self.__running:
+            self.__running = False
+            closing_connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            closing_connection.connect((self.__host, self.__port))
+            closing_connection.close()
+            self.__block.acquire()
+            self.__server_socket.close()
+            self.__block.release()
+        else:
+            print("Server not running!")
+            
+            
+    def send_msg(self, msg):
+        connection.send(msg)
+        
+    def showcords(self, event,x,y,flags,param) -> None:
         xRatio = WIDTH / SWIDTH
         yRatio = HEIGHT / SHEIGHT
         x = str(math.ceil(x*xRatio))
         y = str(math.ceil(y*yRatio))
-        self.socket.send(bytes(x + y, "utf-8"))
+        event = str(event)
+        data = event + " " + x + " " + y
+        self.send_msg(data.encode('utf-8'))
         win32api.SetCursor(win32api.LoadCursor(0, win32con.IDC_HAND))
-        return event
 
-    def showscreen(self, img) -> None:
-        ''' Decompresses and displays on screen '''
-        # decompress = zlib.decompress(img)
-        nparr = np.frombuffer(img, np.uint8)
-        frame = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        frame = cv2.resize(frame, (SWIDTH, SHEIGHT))
-        cv2.imshow('frame', frame)
-        cv2.setMouseCallback("frame", self.showcords)
-        cv2.waitKey(1)
+    def __client_connection(self, connection, address):
+        """
+        Handles the individual client connections and processes their stream data.
+        """
+        payload_size = struct.calcsize('>L')
+        data = b""
 
-    def connect(self) -> None:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.bind((self.ip, self.port))
-        sock.listen(1)
-        self.socket, _ = sock.accept()
-        print("CLient connected!")
-        self.socket.send(b"Welcome")
-        while True:
-            data = self.recvdata()
-            self.showscreen(data)
+        while self.__running:
+            break_loop = False
+            while len(data) < payload_size:
+                received = connection.recv(4096)
+                if received == b'':
+                    connection.close()
+                    self.__used_slots -= 1
+                    break_loop = True
+                    break
+                data += received
 
+            if break_loop:
+                break
+            packed_msg_size = data[:payload_size]
+            data = data[payload_size:]
 
+            msg_size = struct.unpack(">L", packed_msg_size)[0]
+
+            while len(data) < msg_size:
+                data += connection.recv(4096)
+
+            frame_data = data[:msg_size]
+            data = data[msg_size:]
+
+            frame = pickle.loads(frame_data, fix_imports=True, encoding="bytes")
+            frame = cv2.imdecode(frame, cv2.IMREAD_COLOR)
+            frame = cv2.resize(frame, (SWIDTH, SHEIGHT))
+            cv2.imshow(str(address), frame)
+            cv2.setMouseCallback(str(address), self.showcords)
+            cv2.waitKey(1)
+            # if cv2.waitKey(1) == ord(self.__quit_key):
+            #     connection.close()
+            #     self.__used_slots -= 1
+            #     break
 if __name__ == '__main__':
-    servercontrol()
+    server = StreamingServer('0.0.0.0', 443)
+    server.start_server()
